f265d00fda220d6112fb712eb83893b52e4da510
Pablo Prieto <pablotsdaw@gmail.com>
2018-02-15 13:43:58 +0100

    heroku

--- Minijuegos/server/index.js
+++ Minijuegos/server/index.js
@@ -18,7 +18,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, './public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
